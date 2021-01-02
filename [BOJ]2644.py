75622663a99d1a8b8114363a640ec8b2a1a97ad6
K <ways2kim@gmail.com>
2021-01-02 14:43:47 +0900

    20210102

--- [BOJ]2644.py
+++ [BOJ]2644.py
@@ -1,6 +1,31 @@
+from collections import deque
 import sys
+input = sys.stdin.readline
 
-n = int(sys.stdin.readline())
-target_1, target_2 = map(int, sys.stdin.readline().split())
-m = int(sys.stdin.readline())
+n = int(input())
+start, end = map(int, input().split())
+m = int(input())
 
+relation = [[] for _ in range(n+1)]
+dist = [0] * (n+1)
+
+for i in range(m):
+    parent, child = map(int, input().split())
+    relation[parent].append(child)
+    relation[child].append(parent)
+
+def bfs():
+    q = deque()
+    q.append(start)
+    while q:
+        now = q.popleft()
+        for i in relation[now]:
+            if dist[i] == 0:
+                q.append(i)
+                dist[i] = dist[now] + 1
+            if i == end:
+                return dist[i]
+    return -1
+
+print(bfs())
+print(relation)
